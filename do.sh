756969abae28c95b89d874e93dc1fb8f41b849bf
Adi Linden <adi@adis.ca>
2016-03-07 14:29:21 -0600

    Utilized two datastores for backup files and alternates backups between datastores for odd and even days. Also keeps weekly backups.

--- do.sh
+++ do.sh
@@ -2,87 +2,94 @@
 #
 # Execute our backup
 #
-# * April 15, 2012
-# Modified to utilize alternate backup locations
+# * May 9, 2014
+# Modified to take advantage of my custom command line options.
+# Now we can have multiple hosts files and a single script to
+# execute.
 #
 
-myOddDaySub="Daily Odd Backup"
-myOddDayDS="backup.store1"
-myOddDayDir="daily_odd"
-myOddDayCnt="2"
-myEvenDaySub="Daily Even Backup"
-myEvenDayDS="backup.store2"
-myEvenDayDir="daily_even"
-myEvenDayCnt="2"
+myDryrun=0
 
-myOddWeekSub="Weekly Odd Backup"
-myOddWeekDS="backup.store1"
-myOddWeekDir="weekly_odd"
-myOddWeekCnt="3"
-myEvenWeekSub="Weekly Even Backup"
-myEvenWeekDS="backup.store2"
-myEvenWeekDir="weekly_even"
-myEvenWeekCnt="3"
+myOddBackupDatastore="backup1.nfs1"
+myEvenBackupDatastore="backup2.nfs1"
 
-myPath=/home/vi-admin/ghettoVCBg2
-myScript="ghettoVCBg2.pl"
-myLog="/tmp/ghettoVCBg2.log"
-#myArgs="--dryrun 0 --config_dir configs --vmlist hosts --output ${myLog}"
-myArgs="--dryrun 0 --config_dir configs --vmlist hosts.debug --output ${myLog}"
+myDailyBackupDirectory="backup_daily"
+myWeeklyBackupDirectory="backup_weekly"
 
-function prep {
-    # Wipe log file
-    > ${myLog}
+myDailyBackupCount=2
+myWeeklyBackupCount=2
 
-    # Change working directory
-    cd ${myPath}
+# Misc args
+
+myDir=`dirname $0`
+myScript=ghettoVCBg2.pl
+
+# Command line args
+
+function usage {
+    echo "Usage: do.sh {suffix} [reverse]"
+    echo "    suffix       The suffix to append to the hosts file"
+    echo "                 listing all VM to be backed up."
+    echo "    reverse      Reverse the odd and even datastores."
+    echo
+    echo "Written by Adi Linden <adi@adis.on.ca>"
+    echo
 }
 
-# Determine odd or even week
-WK=`date +%W`
-ODD_WK=`expr $WK % 2`
+# What
+mySuffix="$1"
+myList="hosts.$mySuffix"
+if [ ! -e "$myDir/$myList" ]; then
+    echo "Missing VM list file."
+    usage
+    exit 1
+fi
 
-# Determine odd or even day
-DY=`date +%j`
-ODD_DY=`expr $DY % 2`
+# Reversed
+if [ "x$2" = "xreverse" ]; then
+    myReverse="yes"
+fi
 
-# Execute backup
-case "$1" in
-    "daily")
-        prep
-        if [ $ODD_DY = 1 ]; then
-            ${myPath}/${myScript} ${myArgs} \
-                --backup_datastore "${myOddDayDS}" \
-                --backup_directory "${myOddDayDir}" \
-                --backup_rotation ${myOddDayCnt} \
-                --email_subject "${myOddDaySub}"
-        else
-            ${myPath}/${myScript} ${myArgs} \
-                --backup_datastore "${myEvenDayDS}" \
-                --backup_directory "${myEvenDayDir}" \
-                --backup_rotation ${myEvenDayCnt} \
-                --email_subject "${myEvenDaySub}"
-        fi
-        ;;
-    "weekly")
-        prep
-        if [ $ODD_WK = 1 ]; then
-            ${myPath}/${myScript} ${myArgs} \
-                --backup_datastore "${myOddWeekDS}" \
-                --backup_directory "${myOddWeekDir}" \
-                --backup_rotation ${myOddWeekCnt} \
-                --email_subject "${myOddWeekSub}"
-        else
-            ${myPath}/${myScript} ${myArgs} \
-                --backup_datastore "${myEvenWeekDS}" \
-                --backup_directory "${myEvenWeekDir}" \
-                --backup_rotation ${myEvenWeekCnt} \
-                --email_subject "${myEvenWeekSub}"
-        fi
-        ;;
-    *)
-        echo $"Usage: $0 {daily|weekly}"
-        exit 1
-        ;;
-esac
+# When
+myDay=$(date +%j)
+myWeek=$(date +%W)
+myWeekday=$(date +%u)
+myOddDay=$(expr $myDay % 2)
+myOddWeek=$(expr $myWeek % 2)
 
+if [ $myWeekday -eq 6 ]; then
+    myWhen=weekly
+    myBackupDirectory="$myWeeklyBackupDirectory"
+    myBackupCount="$myWeeklyBackupCount"
+    if [ "$myOddWeek" = "1" -a "x$myReverse" = "xyes" ] || [ "$myOddWeek" = "0" -a "x$myReverse" = "x" ]; then
+        myBackupDatastore="$myEvenBackupDatastore"
+    else
+        myBackupDatastore="$myOddBackupDatastore"
+    fi
+else
+    myWhen=daily
+    myBackupDirectory="$myDailyBackupDirectory"
+    myBackupCount="$myDailyBackupCount"
+    if [ "$myOddDay" = "1" -a "x$myReverse" = "xyes" ] || [ "$myOddDay" = "0" -a "x$myReverse" = "x" ]; then
+        myBackupDatastore="$myEvenBackupDatastore"
+    else
+        myBackupDatastore="$myOddBackupDatastore"
+    fi
+fi
+
+
+# Build some more vars
+myEmailSubject="VM Backup - ${mySuffix} - ${myWhen} - ${myBackupDatastore} -"
+myLog="/tmp/ghettoVCBg2-${mySuffix}-${myWhen}-${myBackupDatastore}.log"
+
+# Run backup script
+[ -e "${myLog}" ] && rm -f ${myLog}
+cd ${myDir}
+
+${myDir}/${myScript} --config_dir configs --dryrun ${myDryrun} \
+    --vmlist ${myList} --backup_datastore ${myBackupDatastore} \
+    --backup_directory ${myBackupDirectory} \
+    --backup_rotation ${myBackupCount} \
+    --output ${myLog} --email_subject "${myEmailSubject}"
+
+# End
